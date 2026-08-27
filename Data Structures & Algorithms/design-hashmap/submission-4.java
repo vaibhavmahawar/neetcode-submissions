080dccfe5ab901377ac4247cc7b613b357e44cb8
VAIBHAV KUMAR MAHAWAR <78339845+vaibhavmahawar@users.noreply.github.com>
2026-08-28 01:23:41 +0530

    Add: design-hashmap - submission-4

---
+++ Data Structures & Algorithms/design-hashmap/submission-4.java
@@ -0,0 +1,78 @@
+class ListNode {
+    int key;
+    int value;
+    ListNode next;
+
+    public ListNode(int key, int value, ListNode next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+    
+    public ListNode() {
+        this(0, 0, null);
+    }
+}
+
+class MyHashMap {
+    private ListNode[] map;
+
+    public MyHashMap() {
+        map = new ListNode[10000];
+
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new ListNode();
+        }
+    }
+
+    private int idx(int key) {
+        return key % map.length;
+    }
+    
+    public void put(int key, int value) {
+        ListNode cur = map[idx(key)];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next.value = value;
+                return;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = new ListNode(key, value, null);
+    }
+    
+    public int get(int key) {
+        ListNode cur = map[idx(key)].next;
+
+        while (cur != null) {
+            if (cur.key == key) {
+                return cur.value;
+            }
+            cur = cur.next;
+        }
+
+        return -1;
+    }
+    
+    public void remove(int key) {
+        ListNode cur = map[idx(key)];
+
+        while (cur != null && cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
