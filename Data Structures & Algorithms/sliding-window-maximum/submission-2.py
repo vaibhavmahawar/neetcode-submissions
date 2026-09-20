fea7405ed6d8aaeaccdb54aefc446efc1df95aef
VAIBHAV KUMAR MAHAWAR <78339845+vaibhavmahawar@users.noreply.github.com>
2026-09-20 15:51:10 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.py
@@ -0,0 +1,38 @@
+class SegmentTree:
+    def __init__(self, N, A):
+        self.n = N
+        while (self.n & (self.n - 1)) != 0:
+            self.n += 1
+        self.build(N, A)
+
+    def build(self, N, A):
+        self.tree = [float('-inf')] * (2 * self.n)
+        for i in range(N):
+            self.tree[self.n + i] = A[i]
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = max(self.tree[i << 1], self.tree[i << 1 | 1])
+
+    def query(self, l, r):
+        res = float('-inf')
+        l += self.n
+        r += self.n + 1
+        while l < r:
+            if l & 1:
+                res = max(res, self.tree[l])
+                l += 1
+            if r & 1:
+                r -= 1
+                res = max(res, self.tree[r])
+            l >>= 1
+            r >>= 1
+        return res
+
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        n = len(nums)
+        segTree = SegmentTree(n, nums)
+        output = []
+        for i in range(n - k + 1):
+            output.append(segTree.query(i, i + k - 1))
+        return output
